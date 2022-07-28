ef8e7c8678911116f741123153d8e3d5847e26e3
adarsh <adarsh0ranjan@gmail.com>
2022-07-28 16:19:17 +0530

    Initial Commit

---
+++ project-2/project_2App.swift
@@ -0,0 +1,17 @@
+//
+//  project_2App.swift
+//  project-2
+//
+//  Created by Roro Solutions on 28/07/22.
+//
+
+import SwiftUI
+
+@main
+struct project_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
